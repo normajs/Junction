92a56d9e18b5a8de935025630302a912f2555e3f
richarddubay <richardtdubayjr@gmail.com>
2015-02-10 11:54:05 -0500

    Removed js directory (I have no idea how that ended up there) and changed a browsersync setting in the norma.json.

--- tests/js/utilities/isElement.js
+++
@@ -1,7 +0,0 @@
-junction.isElement = function (el) {
-    if (typeof HTMLElement === "object") {
-        return el instanceof HTMLElement;
-    } else {
-        return el && typeof el === "object" && el !== null && el.nodeType === 1 && typeof el.nodeName === "string";
-    }
-};
