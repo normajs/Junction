f83bfd3c54746933da191135dba89b0791c3da62
richarddubay <richardtdubayjr@gmail.com>
2015-04-20 12:53:20 -0400

    removed profile from publish script

--- publish.sh
+++ publish.sh
@@ -3,4 +3,5 @@ DEFAULT="circleci"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=assets.newspring.cc
 DIR=./junction.js
-aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+# aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws  s3  sync $DIR s3://$BUCKET/
