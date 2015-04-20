8fb195ee61cce4635dd1ed37bd9fdd9bca36a07e
richarddubay <richardtdubayjr@gmail.com>
2015-04-20 16:30:52 -0400

    change where the file was searching

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 DEFAULT="circleci"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=junction.newspring.cc
-DIR=junction.js
+DIR=.
 # aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
-aws  s3  sync $DIR s3://$BUCKET/
+aws  s3  sync $DIR/junction.js s3://$BUCKET/
