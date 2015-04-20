f6d80520feff859ad3c3bc72159c6c7c355676df
richarddubay <richardtdubayjr@gmail.com>
2015-04-20 16:40:16 -0400

    another approach

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=junction.newspring.cc
 DIR=.
 # aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
-aws  s3  sync $DIR/junction.js s3://$BUCKET/
+aws s3 sync ./junction.js s3://$BUCKET/
