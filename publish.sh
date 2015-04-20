a627fabeac51481b0c8b5b2badd45d0097e0d7f0
richarddubay <richardtdubayjr@gmail.com>
2015-04-20 16:20:54 -0400

    another different directory

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@
 DEFAULT="circleci"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=junction.newspring.cc
-DIR=./junction/junction.js
+DIR=junction.js
 # aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
 aws  s3  sync $DIR s3://$BUCKET/
