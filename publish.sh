66a8c206d476d971ab7ccfeaa0135a865c004299
richarddubay <richardtdubayjr@gmail.com>
2015-04-20 17:07:11 -0400

    made circle.yml the only file needed.

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-DEFAULT="circleci"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=junction.newspring.cc
-DIR=.
-# aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
-aws s3 sync ./junction.js s3://$BUCKET/
