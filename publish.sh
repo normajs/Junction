32fdbbae60210cf8e054e89783a6d82b4123617b
richarddubay <richardtdubayjr@gmail.com>
2015-04-20 15:17:21 -0400

    changed buckets

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DEFAULT="circleci"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
-BUCKET=assets.newspring.cc
+BUCKET=junction.newspring.cc
 DIR=./junction.js
 # aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
 aws  s3  sync $DIR s3://$BUCKET/
