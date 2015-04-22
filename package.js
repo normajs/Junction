e83a69bee6f6f94ddffe9c714e5f4da0d59f3266
Ben Wiley <ben.wiley@newspring.cc>
2015-04-22 11:35:59 -0400

    Add readme and git link

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Manipulate the DOM using CSS selectors",
-  version: '1.0.0',
+  version: "1.0.0",
   name: "newspring:junction",
+  git: "https://github.com/NewSpring/junction.git",
+  documentation: "README.md"
 });
 
 Package.onUse(function (api) {
-  api.addFiles(['junction.js'], 'client');
-
-  // api.export('$', 'client');
-  api.export('junction', 'client');
+  api.addFiles("junction.js", "client");
+  api.export("junction", "client");
 });
